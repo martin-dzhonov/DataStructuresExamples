4c37d3478aaa950e485130c5df2e4fe38ce92bf6
Martin Dzhonov <martin.dzhonov@gmail.com>
2015-01-21 22:24:55 +0200

    Adjacency List Graph

---
+++ Graphs/AdjacencyListGraph.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class AdjacencyListGraph<T>
+    {
+        private List<GraphNode<T>> nodes;
+
+        public AdjacencyListGraph()
+        {
+            this.nodes = new List<GraphNode<T>>();
+        }
+
+        public void AddNode(GraphNode<T> node)
+        {
+            this.nodes.Add(node);
+        }
+        public void AddDirectedEdge(GraphNode<T> from, GraphNode<T> to, int cost)
+        {
+            from.Neighbors.Add(to);
+            from.Costs.Add(cost);
+        }
+
+        public void AddUndirectedEdge(GraphNode<T> from, GraphNode<T> to, int cost)
+        {
+            from.Neighbors.Add(to);
+            from.Costs.Add(cost);
+
+            to.Neighbors.Add(from);
+            to.Costs.Add(cost);
+        }
+
+        public bool Contains(T value)
+        {
+            return nodes.Any(n => n.Value.Equals(value));
+        }
+
+    }
+}
---
+++ Graphs/GraphNode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    public class GraphNode<T>
+    {
+        public T Value { get; set; }
+
+        public List<GraphNode<T>> Neighbors { get; set; }
+
+        public List<int> Costs { get; set; }
+
+        public GraphNode(T value)
+        {
+            this.Value = value;
+            this.Neighbors = new List<GraphNode<T>>();
+            this.Costs = new List<int>();
+        }
+
+    }
+}
